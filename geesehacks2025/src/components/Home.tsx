635d174a78a53e632216406a7cdcb62d84520c7f
Danny <79547297+Danh295@users.noreply.github.com>
2025-01-26 02:00:52 -0500

    Added Insurance Coverage Card

--- geesehacks2025/src/components/Home.tsx
+++ geesehacks2025/src/components/Home.tsx
@@ -6,12 +6,28 @@ import ChallengeCard from './ChallengeCard';
 export default function Home() {
   return (
     <div>
-      {/* Current Stats */}
-
-      {/* Challenges */}
-
-      {/* Streak & Points */}
-
+      <InsuranceCard
+        amount={10000}
+        link="https://www.sunnet.sunlife.com/signin/csi/checkcoverage/e/home.wca?"
+      />
     </div>
   )
+}
+
+type InsuranceCardProps = {
+  amount: number;
+  link?: string; // optional
+}
+
+const InsuranceCard: React.FC<InsuranceCardProps> = ({ amount, link = "#" }) => {
+  return (
+    <a
+      href={link}
+      className="bg-primary-yellow text-white rounded-2xl shadow-lg p-6 max-w-sm"
+    >
+      <p>Available Coverage</p>
+      <h1>${amount}</h1>
+      <p>See details</p>
+    </a>
+  );
 }
